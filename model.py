8ffc0cf35449956b7bda85d899ede9ffc4a05151
Doug Neal <doug@neal.me.uk>
2018-07-27 14:20:03 +0100

    Remove default image height/width and default Crop to False
    Let's assume we want the output images to be the same size as the input
    images and we don't want to crop them.
    
    When crop is set to True, the image dimensions are set to the output
    dimensions.

--- model.py
+++ model.py
@@ -13,9 +13,14 @@ from utils import *
 def conv_out_size_same(size, stride):
   return int(math.ceil(float(size) / float(stride)))
 
+# Thoughts:  (DN)
+#  - Remove the cropping, make the output width/height always be the input w/h.
+#  - Chop out the data loading parts and replace them with a FITS loader
+#  - Rename some of these arguments to be a bit less cryptic
+
 class DCGAN(object):
-  def __init__(self, sess, input_height=108, input_width=108, crop=True,
-         batch_size=64, sample_num = 64, output_height=64, output_width=64,
+  def __init__(self, sess, input_height, input_width, crop=False,
+         batch_size=64, sample_num = 64, output_height, output_width,
          y_dim=None, z_dim=100, gf_dim=64, df_dim=64,
          gfc_dim=1024, dfc_dim=1024, c_dim=3, dataset_name='default',
          input_fname_pattern='*.jpg', checkpoint_dir=None, sample_dir=None, data_dir='./data'):
